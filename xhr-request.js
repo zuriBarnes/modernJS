3fc9f4096db0a84e0b07e9b55fb5de75e3d0a15d
Zuri Barnes <45982373+zuriBarnes@users.noreply.github.com>
2021-06-05 23:16:29 -0400

    Update xhr-request.js
    added state error message

--- xhr-request.js
+++ xhr-request.js
@@ -4,7 +4,9 @@ xhr.addEventListener('readystatechange', () => {
 // console.log(xhr, xhr.readyState)
     if(xhr.readyState === 4) {
     console.log(xhr.responseText)
+    } else {
+    console.log('error, loading incomplete')
 }
 })
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+xhr.open('GET', 'https://pokeapi.co/api/v2/')
 xhr.send()
