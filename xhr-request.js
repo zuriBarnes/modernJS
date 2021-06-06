e8f30579a57a531a6dd84817029192c8111127db
Zuri Barnes <45982373+zuriBarnes@users.noreply.github.com>
2021-06-05 23:23:44 -0400

    Update xhr-request.js
    reformatting

--- xhr-request.js
+++ xhr-request.js
@@ -1,12 +1,11 @@
 // Request Data from API Endpoint
 const xhr = new XMLHttpRequest()
-xhr.addEventListener('readystatechange', () => {
-// console.log(xhr, xhr.readyState)
-    if(xhr.readyState === 4) {
-    console.log(xhr.responseText)
-    } else {
-    console.log('error, loading incomplete')
-}
-})
+    xhr.addEventListener('readystatechange', () => {
+        if(xhr.readyState === 4) {
+            console.log(xhr.responseText) 
+        } else {
+            console.log('error, loading incomplete')
+        }
+    })
 xhr.open('GET', 'https://pokeapi.co/api/v2/')
 xhr.send()
