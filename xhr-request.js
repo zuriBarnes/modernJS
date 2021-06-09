5544521a12009df674a66f764bee40547fb8d00f
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-09 09:51:09 -0400

    Update xhr-request.js
    added comments, and readyState conditionals

--- xhr-request.js
+++ xhr-request.js
@@ -1,8 +1,11 @@
-let xhr_1 = new XMLHttpRequest()
-xhr_1.addEventListener('readystatechange', () => {
-    if(xhr_1.readyState === 4) {
-    console.log(xhr_1, xhr_1.responseText)
-} 
+let request = new XMLHttpRequest() // create new XMLHttp Object
+request.addEventListener('readystatechange', () => { // add event listener for State change
+    if(request.readyState === 4 && request.status === 200) { // condition created to print only completed data fetch
+    console.log(request, request.responseText)
+} else if(request.readyState === 4) { // if not 200 status, print msg and error code
+    console.log('Could not fecth data', request.status)
+    }
 })
-xhr_1.open('GET', 'https://api.chucknorris.io/jokes/random')
-xhr_1.send()
+request.open('GET', 'https://api.chucknorris.io/jokes/random/') // main function that talks to API's
+request.send() // send request
+
