92f460ee45050190abda7e6cd505ada347da7f68
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-10 19:39:51 -0400

    Add files via upload

---
+++ includes().js
@@ -0,0 +1,8 @@
+/*
+includes()
+Determines if an array includes a certain element. returns true if it does, false otherwise
+*/
+const people = ['John', 'Mary', 'Caitlyn']
+people.includes('George') // false
+people.includes('John') // true
+
