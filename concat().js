c1cdc6fb14ba4bb644819de8aa098d381a00c3de
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-10 19:50:12 -0400

    Add files via upload

---
+++ concat().js
@@ -0,0 +1,12 @@
+/* 
+concat()
+It merges two or more arrays or values. It doesnt change the existing array. 
+It returns a new array
+*/
+
+const fruits = ['orange', 'banana']
+const vegetables = ['beans', 'broccoli']
+
+const fruitsAndVeggies = fruits.concat(vegetables)
+
+// fruitsAndVeggies = ['orange', 'banana', 'beans', 'broccoli']
