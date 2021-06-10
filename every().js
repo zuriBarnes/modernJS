cef33164285b88b9cfe523a2b4144ba4caff73f1
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-10 10:25:42 -0400

    modern function added
    code provided by: George Moller @_georgemoller

---
+++ every().js
@@ -0,0 +1,83 @@
+/*
+every() 
+Returns true if all the elements in the array pass the "test" implemented by the function false oterwise
+*/
+
+const peopleList = [
+    {name: 'Misty', age: 41},
+    {name: 'Zuri', age: 43},
+    {name: 'Nevan', age: 18},
+    {name: 'Jaden', age: 13}
+]
+
+const areAboveAge = peopleList.every((person => {
+    if(person.age > 20) {
+        return true
+    }
+    return false
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
