fce5316d8aeeb1ce6860418c866715a203a9f25e
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-17 19:54:20 -0400

    Update xhr-request.js
    added Callbacks

--- xhr-request.js
+++ xhr-request.js
@@ -1,11 +1,26 @@
-let request = new XMLHttpRequest() // create new XMLHttp Object
+const getJokes = (callback) => {
+  const request = new XMLHttpRequest() // create new XMLHttp Object
 request.addEventListener('readystatechange', () => { // add event listener for State change
     if(request.readyState === 4 && request.status === 200) { // condition created to print only completed data fetch
-    console.log(request, request.responseText)
+    callback(undefined, request.responseText);
 } else if(request.readyState === 4) { // if not 200 status, print msg and error code
-    console.log('Could not fecth data', request.status)
+    callback('Could not fetch data', undefined);
     }
 })
 request.open('GET', 'https://api.chucknorris.io/jokes/random/') // main function that talks to API's
 request.send() // send request
+  
+}
+console.log(1) // check if callback works
+console.log(2)
 
+getJokes((err, data) => {
+  console.log('callback fired')
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(data)
+  }
+})
+console.log(3)
+console.log(4)
