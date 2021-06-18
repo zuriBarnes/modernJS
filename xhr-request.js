22c73a3db34fa1d9d84fe9b76bec4980847742a9
Zuri <45982373+zuriBarnes@users.noreply.github.com>
2021-06-17 20:17:41 -0400

    Update xhr-request.js
    function name change, and JSON.parse()

--- xhr-request.js
+++ xhr-request.js
@@ -1,20 +1,21 @@
-const getJokes = (callback) => {
+const getData = (callback) => {
   const request = new XMLHttpRequest() // create new XMLHttp Object
-request.addEventListener('readystatechange', () => { // add event listener for State change
+  request.addEventListener('readystatechange', () => { // add event listener for State change
     if(request.readyState === 4 && request.status === 200) { // condition created to print only completed data fetch
-    callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data); // callthe callback function, and add arguements
 } else if(request.readyState === 4) { // if not 200 status, print msg and error code
     callback('Could not fetch data', undefined);
     }
 })
-request.open('GET', 'https://api.chucknorris.io/jokes/random/') // main function that talks to API's
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/') // main function that talks to API's
 request.send() // send request
   
 }
 console.log(1) // check if callback works
 console.log(2)
 
-getJokes((err, data) => {
+getData((err, data) => {
   console.log('callback fired')
   if(err) {
     console.log(err);
