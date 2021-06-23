c7146909fec4e5d5aa302f1736a3a88729c5359f
Zuri Barnes <45982373+zuriBarnes@users.noreply.github.com>
2021-06-22 22:39:24 -0400

    Day_2 code

---
+++ day_2.js
@@ -0,0 +1,11 @@
+const Day_2 = () => {
+    return (
+        <div className="img-container">
+            <h3>Test</h3>
+            <img src="//unsplash.it/804/383" />
+        </div>
+
+    )
+}
+
+export default Day_2
