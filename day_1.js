7fb9278348c2cbe8ce7857d03af898d4f297c808
Zuri Barnes <45982373+zuriBarnes@users.noreply.github.com>
2021-06-22 22:38:10 -0400

    Day 1 Code

---
+++ day_1.js
@@ -0,0 +1,11 @@
+const Day1 = () => {
+    return (
+        <section>
+            <div>
+                <h2 className="title">Day 1 component</h2>
+                <p> This is a react component </p>
+            </div>
+        </section>
+    )
+}
+export default Day1
