6c54c8eb6f9f9e19a71748a6406a56d5172c6109
Zuri Barnes <45982373+zuriBarnes@users.noreply.github.com>
2021-06-24 20:31:34 -0400

    Create componentPractice_1.js

---
+++ componentPractice_1.js
@@ -0,0 +1,22 @@
+// import Header from './Components/Header.js'
+// import TestComp from './Components/TestComp.js';
+// import Button from './Components/Button'
+
+function App() {
+  const myName = 'Zuri'
+  // let likes = 234
+  return (
+  
+    <div className="container">
+      <h1>{ myName }</h1>
+      <p>Liked {Math.floor(Math.random() * 1000) } times</p>
+      {/* <Header title="Task Tracker" />
+      <TestComp />
+      <Button />
+      <TestComp /> */}
+  
+    </div>
+
+  );
+}
+export default App;
